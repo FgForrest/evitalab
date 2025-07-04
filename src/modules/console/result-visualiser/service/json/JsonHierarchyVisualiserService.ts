752a1d546e67b064a8ce192fe757f6ab7315b564
Ondřej Mikolášek <o.miko@seznam.cz>
2025-07-04 18:37:18 +0200

    fix: Fixed problem with displaying props in menu. TODO: Need to check all Immutable.Map.set because set returns new instance Refs: #340

--- src/modules/console/result-visualiser/service/json/JsonHierarchyVisualiserService.ts
+++ src/modules/console/result-visualiser/service/json/JsonHierarchyVisualiserService.ts
@@ -20,31 +20,37 @@ export abstract class JsonHierarchyVisualiserService<VS extends JsonResultVisual
         this.visualiserService = visualiserService
     }
 
-    findNamedHierarchiesByReferencesResults(hierarchyResult: Result,
-                                            entitySchema: EntitySchema): [(ReferenceSchema | undefined), Result][] {
+    findNamedHierarchiesByReferencesResults(
+        hierarchyResult: Result,
+        entitySchema: EntitySchema
+    ): [(ReferenceSchema | undefined), Result][] {
         const referencesWithHierarchies: [ReferenceSchema | undefined, Result][] = []
+
         for (const referenceName of Object.keys(hierarchyResult)) {
             const namedHierarchiesResult: Result = hierarchyResult[referenceName]
-            const convertedNamedHierarchiesResult: ImmutableMap<string, Result> = ImmutableMap()
+
+            let convertedNamedHierarchiesResult: ImmutableMap<string, Result> = ImmutableMap()
             for (const name of Object.keys(namedHierarchiesResult)) {
                 const hierarchy: Result = namedHierarchiesResult[name]
-                convertedNamedHierarchiesResult.set(name, hierarchy)
+                convertedNamedHierarchiesResult = convertedNamedHierarchiesResult.set(name, hierarchy)
             }
 
             if (referenceName === 'self') {
-                referencesWithHierarchies.push([undefined, ImmutableMap(convertedNamedHierarchiesResult)])
+                referencesWithHierarchies.push([undefined, convertedNamedHierarchiesResult])
             } else {
                 const referenceSchema: ReferenceSchema | undefined = entitySchema.references
-                    .find(reference => reference.nameVariants
-                        .get(NamingConvention.CamelCase) === referenceName)
-                if (referenceSchema == undefined) {
+                    .find(reference => reference.nameVariants.get(NamingConvention.CamelCase) === referenceName)
+
+                if (referenceSchema === undefined) {
                     throw new UnexpectedError(`Reference '${referenceName}' not found in entity '${entitySchema.name}'.`)
                 }
-                referencesWithHierarchies.push([referenceSchema, ImmutableMap(convertedNamedHierarchiesResult)])
+
+                referencesWithHierarchies.push([referenceSchema, convertedNamedHierarchiesResult])
             }
         }
         return referencesWithHierarchies
     }
+
 
     abstract resolveNamedHierarchy(namedHierarchyResult: Result[], entityRepresentativeAttributes: string[]): VisualisedNamedHierarchy
 }
