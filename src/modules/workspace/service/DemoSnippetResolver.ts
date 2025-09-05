744178ef0b0f11f9ba7224a4d1442ff0364dbe1c
Lukáš Hornych <lukashornych@proton.me>
2025-09-05 08:17:06 +0200

    fix: demo requests doesn't work after evitaDB stopped supporting custom preconfigured connections

--- src/modules/workspace/service/DemoSnippetResolver.ts
+++ src/modules/workspace/service/DemoSnippetResolver.ts
@@ -1,18 +1,15 @@
 import ky from 'ky'
 import type { InjectionKey } from 'vue'
 import { TabDefinition } from '@/modules/workspace/tab/model/TabDefinition'
-import type { ConnectionId } from '@/modules/connection/model/ConnectionId'
 import type { DemoSnippetRequest } from '@/modules/workspace/tab/model/DemoSnippetRequest'
 import { UnexpectedError } from '@/modules/base/exception/UnexpectedError'
 import { GraphQLInstanceType } from '@/modules/graphql-console/console/model/GraphQLInstanceType'
-import { ConnectionService } from '@/modules/connection/service/ConnectionService'
 import { GraphQLConsoleTabFactory } from '@/modules/graphql-console/console/workspace/service/GraphQLConsoleTabFactory'
 import { EvitaQLConsoleTabFactory } from '@/modules/evitaql-console/console/workspace/service/EvitaQLConsoleTabFactory'
 import { EvitaQLConsoleTabData } from '@/modules/evitaql-console/console/workspace/model/EvitaQLConsoleTabData'
 import { GraphQLConsoleTabData } from '@/modules/graphql-console/console/workspace/model/GraphQLConsoleTabData'
 import { mandatoryInject } from '@/utils/reactivity'
 
-const demoConnectionId: ConnectionId = 'demo'
 const demoCatalog: string = 'evita'
 const baseCodeSnippetUrl: string = 'https://raw.githubusercontent.com/FgForrest/evitaDB'
 
@@ -22,14 +19,11 @@ export const demoSnippetResolverInjectionKey: InjectionKey<DemoSnippetResolver> 
  * Resolves demo code snippet requests from URL into {@link TabRequest}s.
  */
 export class DemoSnippetResolver {
-    private readonly connectionService: ConnectionService
     private readonly evitaQLConsoleTabFactory: EvitaQLConsoleTabFactory
     private readonly graphQLConsoleTabFactory: GraphQLConsoleTabFactory
 
-    constructor(connectionService: ConnectionService,
-                evitaQLConsoleTabFactory: EvitaQLConsoleTabFactory,
+    constructor(evitaQLConsoleTabFactory: EvitaQLConsoleTabFactory,
                 graphQLConsoleTabFactory: GraphQLConsoleTabFactory) {
-        this.connectionService = connectionService
         this.evitaQLConsoleTabFactory = evitaQLConsoleTabFactory
         this.graphQLConsoleTabFactory = graphQLConsoleTabFactory
     }
@@ -47,9 +41,6 @@ export class DemoSnippetResolver {
         } catch (e) {
             throw new UnexpectedError(`Cannot fetch demo code snippet '${request.path}' from GitHub from branch '${request.branch}'.`)
         }
-
-        // validate that connection exists
-        this.connectionService.getConnection(demoConnectionId)
 
         const extension: string = request.path.substring(request.path.lastIndexOf(".") + 1)
         switch (extension) {
