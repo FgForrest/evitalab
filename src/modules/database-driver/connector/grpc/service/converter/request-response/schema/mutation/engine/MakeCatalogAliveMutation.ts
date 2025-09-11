a9c544bff197d28078eaf270990044cc34866ff5
pfi <filip@fg.cz>
2025-09-11 13:26:15 +0200

    refactor: WIP - mutation converters
    Refs: #354

--- src/modules/database-driver/connector/grpc/service/converter/request-response/schema/mutation/engine/MakeCatalogAliveMutation.ts
+++ src/modules/database-driver/connector/grpc/service/converter/request-response/schema/mutation/engine/MakeCatalogAliveMutation.ts
@@ -1,14 +1,14 @@
-import type {
-    MutationConverter
-} from '@/modules/database-driver/connector/grpc/service/converter/request-response/schema/mutation/MutationConverter.ts'
 import {
     MakeCatalogAliveMutation
 } from '@/modules/database-driver/request-response/schema/mutation/engine/MakeCatalogAliveMutation.ts'
 import type {
     GrpcMakeCatalogAliveMutation
 } from '@/modules/database-driver/connector/grpc/gen/GrpcEngineMutation_pb.ts'
+import type {
+    SchemaMutationConverter
+} from '@/modules/database-driver/request-response/schema/mutation/SchemaMutationConverter.ts'
 
-export class MakeCatalogAliveMutationConverter implements MutationConverter<MakeCatalogAliveMutation, GrpcMakeCatalogAliveMutation> {
+export class MakeCatalogAliveMutationConverter implements SchemaMutationConverter<MakeCatalogAliveMutation, GrpcMakeCatalogAliveMutation> {
 
     convert(mutation: GrpcMakeCatalogAliveMutation): MakeCatalogAliveMutation {
         return new MakeCatalogAliveMutation(mutation.catalogName)
