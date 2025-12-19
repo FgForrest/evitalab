eb9286b88d70d735fda2d7d99b5953a6bed5e417
pfi <filip@fg.cz>
2025-12-19 11:10:34 +0100

    fix: CatalogVersionAtResponse structure updated

--- src/modules/database-driver/request-response/CatalogVersionAtResponse.ts
+++ src/modules/database-driver/request-response/CatalogVersionAtResponse.ts
@@ -1,11 +1,13 @@
 import { OffsetDateTime } from '@/modules/database-driver/data-type/OffsetDateTime'
 
 export class CatalogVersionAtResponse {
-    readonly version: bigint
+    readonly startVersion: bigint
+    readonly endVersion: bigint
     readonly introducedAt: OffsetDateTime
 
-    constructor(version: bigint, introducedAt: OffsetDateTime) {
-        this.version = version
+    constructor(startVersion: bigint, endVersion: bigint, introducedAt: OffsetDateTime) {
+        this.startVersion = startVersion
+        this.endVersion = endVersion
         this.introducedAt = introducedAt
     }
 }
