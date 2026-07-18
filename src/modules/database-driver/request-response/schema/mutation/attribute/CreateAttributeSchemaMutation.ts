7dc603885a3260cec03181eb47a44cafcfe20a52
LHO <hornych@fg.cz>
2026-07-18 09:32:36 +0000

    refactor(types): type attribute-schema-mutation type field as Scalar
    any 97 -> 94; typecheck 0.

--- src/modules/database-driver/request-response/schema/mutation/attribute/CreateAttributeSchemaMutation.ts
+++ src/modules/database-driver/request-response/schema/mutation/attribute/CreateAttributeSchemaMutation.ts
@@ -1,3 +1,4 @@
+import type { Scalar } from '@/modules/database-driver/data-type/Scalar'
 import type { EvitaValue } from '@/modules/database-driver/data-type/EvitaValue'
 import {
     type ScopedAttributeUniquenessType
@@ -15,12 +16,12 @@ export class CreateAttributeSchemaMutation {
     readonly localized: boolean
     readonly nullable: boolean
     readonly representative: boolean
-    readonly type: any
+    readonly type: Scalar
     readonly defaultValue: EvitaValue|undefined
     readonly indexedDecimalPlaces: number
 
 
-    constructor(name: string, description: string|undefined, deprecationNotice: string|undefined, uniqueInScopes: ImmutableList<ScopedAttributeUniquenessType>, filterableInScopes: ImmutableList<EntityScope>, sortableInScopes: ImmutableList<EntityScope>, localized: boolean, nullable: boolean, representative: boolean, type: any, defaultValue: EvitaValue|undefined, indexedDecimalPlaces: number) {
+    constructor(name: string, description: string|undefined, deprecationNotice: string|undefined, uniqueInScopes: ImmutableList<ScopedAttributeUniquenessType>, filterableInScopes: ImmutableList<EntityScope>, sortableInScopes: ImmutableList<EntityScope>, localized: boolean, nullable: boolean, representative: boolean, type: Scalar, defaultValue: EvitaValue|undefined, indexedDecimalPlaces: number) {
         this.name = name
         this.description = description
         this.deprecationNotice = deprecationNotice
